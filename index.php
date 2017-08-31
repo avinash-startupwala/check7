32c7708d03cdeeed01d6dc662edae48f0c943747
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-08-31 16:37:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -148,7 +148,7 @@
           
           <button class="button button-block"/>Log In</button>
           
-            <button button type="submit" class="button button-block"/>Log In</button>
+            <button type="submit" class="button button-block"/>Log In</button>
     
           </form>
 
