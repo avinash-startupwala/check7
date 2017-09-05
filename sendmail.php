82d407fc485491bd67f17f33909d29120c19ad59
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-05 13:44:25 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -3,16 +3,16 @@
 
 // If you are using Composer
 require 'vendor/autoload.php';
-echo "welcome to send grid test";
 
-echo "<br>";
-// If you are not using Composer (recommended)
-// require("path/to/sendgrid-php/sendgrid-php.php");
-//avinash.pawar@startupwala.com
-//avinashpawar059@gmail.com
-$from = new SendGrid\Email(null, "avinash.pawar@startupwala.com");
+	class SendMail
+	{
+
+			function sendmail()
+			{
+
+				$from = new SendGrid\Email(null, "avinashpawar059@gmail.com");
 $subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "avinashpawar059@gmail.com");
+$to = new SendGrid\Email(null, "avinash.pawar@startupwala.com");
 $content = new SendGrid\Content("text/plain", "Hello, Email!");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
@@ -20,11 +20,17 @@ $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+// echo $response->statusCode();
+// echo $response->headers();
+// echo $response->body();
 
-echo "<br>";
+// echo "<br>";
 
-echo $response;
+// echo $response;
+				
+				
+			}
+
+
+	}
 ?>
