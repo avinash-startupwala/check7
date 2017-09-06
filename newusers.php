a51d759a0246dd36963d686d54b57ddb414cabcf
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-06 13:13:14 +0530

    Update newusers.php

--- newusers.php
+++ newusers.php
@@ -2,18 +2,19 @@
 require_once("heroku_postgres_database.php");
 $herokupostgrsdatabse = new HerokuPostgresDatabase();
 		//create new table query
-	$create_table_query = "CREATE TABLE  newusers(
-   user_id SERIAL PRIMARY KEY,
-   first_name VARCHAR(50),
-   last_name VARCHAR(50),
+// 	$create_table_query = "CREATE TABLE  newusers(
+//    user_id SERIAL PRIMARY KEY,
+//    first_name VARCHAR(50),
+//    last_name VARCHAR(50),
    
-   phone VARCHAR(15),
-    city VARCHAR(30),
+//    phone VARCHAR(15),
+//     city VARCHAR(30),
     
-     looking_for VARCHAR(100),
-     email VARCHAR(30),
-     password VARCHAR(50),
-     random_key VARCHAR(50))";
+//      looking_for VARCHAR(100),
+//      email VARCHAR(30),
+//      password VARCHAR(50),
+//      random_key VARCHAR(50))";
+
      $random_key =sha1(microtime(true).mt_rand(10000,90000));
 
   $create_table_result =  $herokupostgrsdatabse->query($create_table_query);
