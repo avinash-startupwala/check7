74e0c4657fa08f9b0e36f79c3a63aa7f5c1d7771
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-07 13:03:18 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -15,7 +15,18 @@ require 'vendor/autoload.php';
 				$from = new SendGrid\Email(null, "enquiry@bmcgroup.in");
 $subject = "Confirm your Startupwala registration!";
 $to = new SendGrid\Email(null, $email);
-$content = new SendGrid\Content("text/html", "<html><body><h1>Dear $first_name</h1><a href=https://startupwala.herokuapp.com/confirmuser.php?key=$random_key&&email=$email>Activate your account</a></body></html>");
+$content = new SendGrid\Content("text/html", "<html>
+<body>
+<h1>Dear $first_name</h1>
+<br>
+<h3>
+You're almost done! Please click this link below to activate your Startupwala account and get started.
+</h3>
+<img src="images/logo.png">
+<br>
+<br>
+<a href=https://startupwala.herokuapp.com/confirmuser.php?key=$random_key&&email=$email>Activate your account</a>
+</body></html>");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
