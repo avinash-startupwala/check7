81bf4a532687ff450c5fb96a212cc258baac89cd
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-08 11:43:10 +0530

    Create saveupdatedpassword.php

---
+++ saveupdatedpassword.php
@@ -0,0 +1,11 @@
+//  $password = $_POST['password'];
+// $update_password_query = "UPDATE registered_users SET password = '$password' WHERE email = '$email'";
+// $update_user_data_result =  $herokupostgrsdatabse->query($update_password_query);
+// 		    $delete_password_query = "delete from changepassword_table where email='$email'";
+//       $data = $herokupostgrsdatabse->query($delete_password_query);
+//         //header('Location: https://startupwala.herokuapp.com/thankyou2.html');
+       
+//        }
+// else {
+// echo "you can't access this page, email link is expired";
+// }
