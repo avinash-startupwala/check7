8ec1cf75b7770757cc4cd5131b67258a59632857
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-08 13:11:57 +0530

    Update setnewpassword.php

--- setnewpassword.php
+++ setnewpassword.php
@@ -5,6 +5,19 @@ if (isset($_POST['password']))
 	 $password = $_POST['password'];
 	
 	echo "password = ".$password;
+	
+	  $email = $_GET['email'];
+	echo "<br>";
+	echo $email;
+	
+
+$update_password_query = "UPDATE registered_users SET password = '$password' WHERE email = '$email'";
+$update_user_data_result =  $herokupostgrsdatabse->query($update_password_query);
+		    $delete_password_query = "delete from changepassword_table where email='$email'";
+      $data = $herokupostgrsdatabse->query($delete_password_query);
+        //header('Location: https://startupwala.herokuapp.com/thankyou2.html');
+       echo "your password is changed successfully";
+	
 }
 else
 {
