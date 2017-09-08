9a1c34ad6f719c6000ba48c57a92a683a72e6682
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-08 11:46:08 +0530

    Update saveupdatedpassword.php

--- saveupdatedpassword.php
+++ saveupdatedpassword.php
@@ -1,11 +1,23 @@
-//  $password = $_POST['password'];
-// $update_password_query = "UPDATE registered_users SET password = '$password' WHERE email = '$email'";
-// $update_user_data_result =  $herokupostgrsdatabse->query($update_password_query);
-// 		    $delete_password_query = "delete from changepassword_table where email='$email'";
-//       $data = $herokupostgrsdatabse->query($delete_password_query);
-//         //header('Location: https://startupwala.herokuapp.com/thankyou2.html');
-       
-//        }
-// else {
-// echo "you can't access this page, email link is expired";
-// }
+ 
+<?php
+if isset($_POST['password'])
+{
+       $password = $_POST['password'];
+       $email = $_POST['email'];
+$password = $_POST['password'];
+$update_password_query = "UPDATE registered_users SET password = '$password' WHERE email = '$email'";
+$update_user_data_result =  $herokupostgrsdatabse->query($update_password_query);
+		    $delete_password_query = "delete from changepassword_table where email='$email'";
+      $data = $herokupostgrsdatabse->query($delete_password_query);
+        //header('Location: https://startupwala.herokuapp.com/thankyou2.html');
+       echo "your password is changed successfully";
+       }
+else {
+echo "you can't access this page, email link is expired";
+}
+}
+
+else {
+echo "please enter password";
+}
+?>
