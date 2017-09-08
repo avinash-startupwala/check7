3b48945dbfdf71b3a13ef9ea8b3e17165c71a489
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-08 10:43:04 +0530

    Update setnewpassword.php

--- setnewpassword.php
+++ setnewpassword.php
@@ -13,7 +13,7 @@ $herokupostgrsdatabse = new HerokuPostgresDatabase();
    $check_user_query = "select random_key from changepassword_table where email='$email'";
     $check_user_query_result = $herokupostgrsdatabse->query($check_user_query);
      if (pg_num_rows($check_user_query_result) == 0) {
-     // echo "u cant access this page";
+     echo "u cant access this page email with not matching key ";
      //  echo "<br>";
       // echo "in first if 1";
      }
@@ -24,6 +24,7 @@ $herokupostgrsdatabse = new HerokuPostgresDatabase();
        $random_key_from_db = $row['random_key'];
           //  echo "<br>";
       // echo "random_key_from_db = ".$random_key_from_db;
+     }
        if ($random_key_from_db==$random_key) {
 
   $update_password_query = "UPDATE registered_users SET password = '$password' WHERE email = '$email'";
@@ -36,4 +37,7 @@ $update_user_data_result =  $herokupostgrsdatabse->query($update_password_query)
 
 
        	}
+else {
+echo "you can't access this page, email link is expired";
+}
 ?>
