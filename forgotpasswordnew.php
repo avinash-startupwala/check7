b178673a272c7c6b9124423a7333864f7deb5958
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-08 11:19:39 +0530

    Update forgotpasswordnew.php

--- forgotpasswordnew.php
+++ forgotpasswordnew.php
@@ -43,7 +43,21 @@ else {
 else {
   
 ?>
+  <!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Sign-Up Form</title>
+  <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
   
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
   <div class="form">
       
 
