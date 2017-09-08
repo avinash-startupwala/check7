4da6cc872818594ea72ec97a15fb49b576963a2e
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-08 15:03:28 +0530

    Update setnewpassword.php

--- setnewpassword.php
+++ setnewpassword.php
@@ -1,10 +1,13 @@
 <?php
 error_reporting(E_ALL);
     ini_set('display_errors', 1);
+require_once("heroku_postgres_database.php");
 
 session_start();
 if (isset($_POST['password']))
 {
+	      $herokupostgrsdatabse = new HerokuPostgresDatabase();
+
        $password = $_POST['password'];
 	echo "<br>";
 	echo $password;
