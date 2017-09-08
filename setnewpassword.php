5cfa4d70b3a0237b6207684dc91d0ef2936fbe1e
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-08 11:24:54 +0530

    Update setnewpassword.php

--- setnewpassword.php
+++ setnewpassword.php
@@ -47,47 +47,25 @@ $herokupostgrsdatabse = new HerokuPostgresDatabase();
 
 <body>
   <div class="form">
-      
-
-      
-    
-        
-  <div id="login">   
-          <h1> What is your Email Address?</h1>
-          
-         <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-          
-            <div class="field-wrap">
+ <div id="login">   
+          <h1>New Password</h1>
+ <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<div class="field-wrap">
             <label>
-              Email Address<span class="req">*</span>
+              Enter new password<span class="req">*</span>
             </label>
-            <input type="email" id="email" name="email"  required autocomplete="off"/>
+            <input type="password" id="password" name="password"  required autocomplete="off"/>
           </div>
-         
-          
-
-          <button type="submit" class="button button-block"/>Submit</button>
-          
-          </form>
-
-
-  
-      
+<button type="submit" class="button button-block"/>Submit</button>
+ </form>
 </div> <!-- /form -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-    <script src="js/index.js"></script>
-
+ <script src="js/index.js"></script>
 </body>
 </html>
-
-
 <?php
-	       
-	
-		  $password = $_POST['password'];
-
-  $update_password_query = "UPDATE registered_users SET password = '$password' WHERE email = '$email'";
+ $password = $_POST['password'];
+$update_password_query = "UPDATE registered_users SET password = '$password' WHERE email = '$email'";
 $update_user_data_result =  $herokupostgrsdatabse->query($update_password_query);
 
 		    $delete_password_query = "delete from changepassword_table where email='$email'";
